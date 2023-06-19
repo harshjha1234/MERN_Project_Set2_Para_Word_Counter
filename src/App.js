c21177d3ccff5698b4c56c982d363e5c43325b51
harshjha1234 <74855047+harshjha1234@users.noreply.github.com>
2023-06-19 19:04:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1> Responsive Paragraph Word Counter</h1>
+      <form >
+        <div >
+          <textarea
+            rows={10}
+            value={text}
+            onChange={(e) => {  
+              setText(e.target.value);
+            }}
+      
+          />
+            <p> 
+
+           </p>
+          <label>Your Text</label>
+
+          <div>Word Count: {text.length > 0? text.split(" ").length: 0}</div>
+          <p> </p>
+          <a href="https://github.com/harshjha1234">GitHub</a>
+          <p>  </p>
+          <p>
+
+          </p>
+        </div>
+      </form>
     </div>
   );
 }
